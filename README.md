58004774ecd0289c04c5ad761cb69d8d458e403d
ybv <ybv@batcave.local>
2020-01-21 07:30:28 -0500

    more descriptive readme

---
+++ README.md
@@ -0,0 +1,28 @@
+<a href="https://wiki.dbpedia.org/downloads-2016-10"><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F7%2F73%2FDBpediaLogo.svg%2F1200px-DBpediaLogo.svg.png&f=1&nofb=1" title="dbpedia" alt="dbpedia"></a>
+
+
+# DBpedia person explorer
+
+
+## What this project does
+- Exposes an inerface for adding single/ multiple person <a href="http://downloads.dbpedia.org/preview.php?file=2016-10_sl_core-i18n_sl_en_sl_persondata_en.ttl.bz2">data</a> triples into <a href="https://github.com/cayleygraph/cayley" target="_blank">cayley</a>
+- Exposes an interface to search for indexed person data by name, andlookup other persons by location
+
+
+## Installation 
+- Clone this repository
+- Intall <a href="https://docs.docker.com/install/">docker</a>
+- Pull the backend image, and build frontend image by
+```bash
+cd dbpedia_graph
+./init.sh
+```
+- You should see logs indicating the backend server running at `http://localhost:64210` and the frontend server running at `http://localhost:5000`
+
+
+## License
+
+[![License](http://img.shields.io/:license-mit-blue.svg?style=flat-square)](http://badges.mit-license.org)
+
+- **[MIT license](http://opensource.org/licenses/mit-license.php)**
+
--- init.sh
+++ init.sh
@@ -4,18 +4,30 @@ curr_dir=$PWD
 server_dir=$PWD/server
 client_dir=$PWD/client
 
-docker stop dbepdia_server || true && docker rm dbepdia_server || true
-docker stop dbpedia_client || true && docker rm dbpedia_client || true
+docker stop dbepdia_server &>/dev/null || true 
+docker stop dbpedia_client &>/dev/null || true 
 
+docker rm dbepdia_server &>/dev/null || true
+docker rm dbpedia_client &>/dev/null || true
 
 # start backend server + db by pulling and running cayley image
-echo "Initializing a new database and starting backend server"
-docker run --name dbepdia_server -v $server_dir/data:/data -p 64210:64210 -d cayleygraph/cayley  -c /data/cayley.yml --init
+echo
+echo "===Initializing a new database and starting backend server==="
+echo
+echo
+docker run --name dbepdia_server -v $server_dir/data:/data -p 64210:64210 -d --rm cayleygraph/cayley  -c /data/cayley.yml --init
 
+echo
+echo "Backend server running at http://localhost:64210"
+echo
 
 # build frontend code
-echo "Building frontend code"
-docker build -t dbpedia_person_data_client:dev $client_dir
-docker run --name dbpedia_client -v $client_dir:/app -v $client_dir/node_modules -p 3001:3000 --rm dbpedia_person_data_client:dev
+echo
+echo "===Building frontend code==="
+echo
+echo
 
-# docker run -v $PWD/data:/data -p 64210:64210 -d cayleygraph/cayley  -c /data/cayley.yml --init
+docker build -t dbpedia_person_data_client:dev $client_dir
+
+docker run --name dbpedia_client -v $client_dir:/build -v $client_dir:/src -v $client_dir:/app -v $client_dir/node_modules -p 5000:5000 --rm dbpedia_person_data_client:dev
+
